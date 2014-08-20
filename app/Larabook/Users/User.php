f8696569755ffd3d479d5ba5838f755e0163c900
Joseph Krump <krump.joseph@gmail.com>
2014-08-20 13:18:29 -0700

    override method for setting password attribute added to the Users model so that passwords are always hashed when they are saved

--- app/Larabook/Users/User.php
+++ app/Larabook/Users/User.php
@@ -7,6 +7,7 @@ use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableTrait;
 use Illuminate\Auth\Reminders\RemindableInterface;
 use Eloquent;
+use Hash;
 
 class User extends Eloquent implements UserInterface, RemindableInterface {
 
@@ -43,6 +44,15 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	{
 	    return 'remember_token';
 	}
+    
+    /**
+     * Passwords must always be hashed.
+     * @param type $password
+     */
+    public function setPasswordAttribute($password)
+    {
+        $this->attributes['password'] = Hash::make($password);
+    }
     
     /**
      * Register a new User
